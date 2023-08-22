877dd612a8f06fe9cc02a2a62464ca2393f5ed0f
Haoyan.Li <haoyan.li.ac@gmail.com>
2023-08-22 21:32:59 +0900

    drawarc

--- Kinematics.cs
+++ Kinematics.cs
@@ -14,9 +14,46 @@ namespace ConventionalIK {
         // Kappa: Curvature
         // Phi: angle
         // S: Length
-        public Vector<double> GetPCCParameters(Vector<double> l) {
+        public static Vector<double> GetSingleSectionArcParameters(Vector<double> l, int numOfSegments, double diameter) {
+            var l0 = l[0];
+            var l1 = l[1];
+            var l2 = l[2];
 
-            return Vector<double>.Build.DenseOfArray(new double[] { 0.0, 0.0, 0.0 });
+            // number of segments
+            var n = numOfSegments;
+
+            // diameter of the trunk frame
+            var d = diameter;
+
+            // root of sum of all lengths
+            var f0 = Math.Sqrt(l0 * l0 + l1 * l1 + l2 * l2 - l0 * l1 - l0 * l2 - l1 * l2);
+
+            var s = n * d * (l0 + l1 + l2) / f0 * Math.Asin(f0 / (3 * n * d));
+            var kappa = 2 * f0 / (d * (l0 + l1 + l2));
+            var phi = Math.Atan(Math.Sqrt(3) / 3 * (l2 + l1 - 2 * l0) / (l1 - l2));
+
+            return Vector<double>.Build.DenseOfArray(new double[] { s, kappa, phi });
+        }
+
+        public static Vector<double> GetLengths(Vector<double> arcParameters, int numOfSegments, double diameter) {
+            var s = arcParameters[0];
+            var kappa = arcParameters[1];
+            var phi = arcParameters[2];
+
+            var d = diameter;
+            var n = numOfSegments;
+
+            var l0 = 2 * n * Math.Sin(kappa * s / (2 * n)) * (1 / kappa - d * Math.Sin(phi));
+            var l1 = 2 * n * Math.Sin(kappa * s / (2 * n)) * (1 / kappa + d * Math.Sin(phi + Math.PI / 3));
+            var l2 = 2 * n * Math.Sin(kappa * s / (2 * n)) * (1 / kappa - d * Math.Cos(phi + Math.PI / 6));
+
+            if (kappa == 0) {
+                l0 = s;
+                l1 = s;
+                l2 = s;
+            }
+
+            return Vector<double>.Build.DenseOfArray(new double[] { l0, l1, l2 });
         }
     }
 
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,6 +14,11 @@ using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using HelixToolkit.Wpf;
+using MathNet.Numerics.LinearAlgebra;
+using OpenTK.Mathematics;
+using Quaternion = System.Windows.Media.Media3D.Quaternion;
+using Quaterniond = OpenTK.Mathematics.Quaterniond;
 
 namespace ConventionalIK {
     /// <summary>
@@ -21,13 +26,29 @@ namespace ConventionalIK {
     /// </summary>
     public partial class MainWindow : Window {
         public Point3DCollection ArcPath { get; private set; }
+        public MeshGeometry3D YourMesh { get; private set; }
 
         public MainWindow() {
             InitializeComponent();
             DataContext = this;
 
+            var l = Vector<double>.Build.DenseOfArray(new double[] { 5, 10, 10 });
+
+            var arc = Kinematics.GetSingleSectionArcParameters(l, 1, 5);
+            var s = arc[0];
+            var kappa = arc[1];
+            var phi = arc[2];
+            var radius = 1 / kappa;
+
             // Create a 3D arc path
-            ArcPath = new Point3DCollection(Create3DArcPath(new Point3D(0, 0, 0), 2, 45, 135, 0.1));
+            ArcPath = new Point3DCollection(ComputeArcPoints(new Vector3d(radius, 0, 0), kappa, new Vector3d(0,0,0), s).Select(e => new Point3D(e.X, e.Y, e.Z)));
+
+            var visual = new MeshGeometryVisual3D() {
+                MeshGeometry = CreatePlaneWithNormal(phi*180/Math.PI),
+                Material = Materials.Blue,
+            };
+
+            view1.Children.Add(visual);
         }
 
         private List<Point3D> Create3DArcPath(Point3D center, double radius, double startAngle, double endAngle, double step) {
@@ -40,5 +61,84 @@ namespace ConventionalIK {
             }
             return path;
         }
+
+        //private Point3DCollection CreateTrunk() {
+        //    var origin = new Point3D(0, 0, 0);
+        //    var trunk = new Point3DCollection();
+        //    var l = Vector<double>.Build.DenseOfArray(new double[] { 10, 10, 10 });
+
+        //    var arc = Kinematics.GetSingleSectionArcParameters(l, 1, 5);
+        //    var s = arc[0];
+        //    var kappa = arc[1];
+        //    var phi = arc[2];
+        //    var radius = 1 / kappa;
+
+        //    origin.X = radius;
+
+        //    // Create a 3D arc path from center, radius, startPoint, endPoint and step
+        //    var arcPath = Create3DArcPath(origin, radius, phi * (180 / Math.PI), 180, 0.1);
+        //}
+
+        public List<Vector3d> ComputeArcPoints(Vector3d center, double kappa, Vector3d startPoint, double arcLength) {
+            var arcPoints = new List<Vector3d>();
+            var radius = 1 / kappa;
+            var startVector = startPoint - center;
+
+            // Ensure startVector is normalized
+            startVector.Normalize();
+
+            var theta = 30;
+            // Compute the normal to the arc's plane according to an angle theta in x-y plane
+            //Vector3d normal = new Vector3d(Math.Cos(180 / Math.PI * theta), Math.Sin(180 / Math.PI * theta), 0);
+
+            // Compute the normal to the arc's plane
+            Vector3d normal;
+
+            if (startVector != Vector3d.Cross(startVector, new Vector3d(0, 0, 1)))
+                normal = Vector3d.Cross(startVector, new Vector3d(0, 0, 1));
+            else
+                normal = Vector3d.Cross(startVector, new Vector3d(0, 1, 0));
+
+            normal.Normalize();
+
+            int numSegments = 100;  // The number of segments to use to draw the arc. Adjust as needed.
+            double angleIncrement = arcLength / radius / numSegments;
+
+            for (int i = 0; i <= numSegments; i++) {
+                double alpha = i * angleIncrement;
+                Vector3d rotatedVector = Quaterniond.FromAxisAngle(normal, alpha) * startVector;
+                arcPoints.Add(center + rotatedVector * radius);
+            }
+
+            return arcPoints;
+        }
+
+        public MeshGeometry3D CreatePlaneWithNormal(double angle) {
+            // A unit square in the XY-plane
+            Point3D p1 = new Point3D(0, 0, 0);
+            Point3D p2 = new Point3D(0, 0, 1);
+            Point3D p3 = new Point3D(1, 0, 1);
+            Point3D p4 = new Point3D(1, 0, 0);
+
+            // Find rotation that aligns Z-axis (0, 0, 1) to the given normal
+
+
+            Vector3D zAxis = new Vector3D(0, 0, 1);
+            Quaternion rotation = new Quaternion(zAxis, angle);
+
+            // Rotate the points
+            var rotateTransform = new RotateTransform3D(new QuaternionRotation3D(rotation));
+            p1 = rotateTransform.Transform(p1);
+            p2 = rotateTransform.Transform(p2);
+            p3 = rotateTransform.Transform(p3);
+            p4 = rotateTransform.Transform(p4);
+
+            // Create geometry using MeshBuilder
+            var meshBuilder = new MeshBuilder();
+            meshBuilder.AddQuad(p1, p2, p3, p4);
+
+            return meshBuilder.ToMesh();
+        }
+
     }
 }
